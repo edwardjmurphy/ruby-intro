a75049e11a6cd50172a62cd863f652b651e9bd28
edwardjmurphy <edward.murphy@kellogg.northwestern.edu>
2023-01-24 22:39:32 +0000

    Practice Cont'd

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -3,7 +3,14 @@
 # ruby 4-hashes.rb
 
 # Hashes are lists of *key-value pairs*
+profile = {
+"name" => "Eduardo",
+"location" => "Evanston",
+"status" => "Learning to Code"
+}
 
 # Accessing data from the hash
+
+puts profile ["name"]
 
 # More Complex Hashes
--- labs/4-hashes.rb
+++ labs/4-hashes.rb
@@ -28,5 +28,7 @@ bitcoin = gets.chomp
 # 2. The value will be a string, so you'll want to convert it to a Float.
 bitcoin = bitcoin.to_f
 
+puts bitcoin_data
+
 # 3. inspect the bitcoin_data hash
 # puts bitcoin_data
